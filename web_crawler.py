407e46e5b0f13e181c46094ee45c6035ac0f465c
dnkywin <calvindeng@college.harvard.edu>
2014-02-22 00:17:28 -0500

    basic program outline?

---
+++ web_crawler.py
@@ -0,0 +1,64 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from collections import Queue
+from multiprocessing.pool import ThreadPool
+import threading
+
+def run(globalUrl):
+    """
+    Scrapes all of the links recursively from a given domain name.
+    
+    Arguments:
+    
+    globalUrl - the overall domain name (e.g. 'http://www.python.org')
+    
+    Returns a set of urls
+    
+    """
+    
+     
+    queue = Queue()
+    websites = Queue()
+    visited_websites = set()
+    numWorkers = 4
+    idle = [True]*numWorkers
+    
+    lock = threading.Lock()
+    
+    def worker(ID):
+        
+        while True: 
+            
+            #check if there are any websites left to be processed, and get the top one if necessary
+            with lock:
+                if websites:
+                
+                    url = websites.get()
+                    working[ID] = True
+                elif all(idle): 
+                    #there are no more websites to process, and none of the other threads are adding links either
+                    return
+                else: 
+                    #wait for threads to finish running / more websites to appear
+                    continue
+            
+            page = urlopen(url)
+            soup = BeautifulSoup(page)
+            
+            links = []
+            
+            #run code here
+            
+            with lock:
+                for link in links:
+                    if link not in visited_websites:
+                        visited_websites.add(link)
+                        websites.put(link)
+                working[ID] = False
+                
+    websites.put(globalUrl)
+    
+    Threadool(numWorkers).imap_unordered(worker, range(numWorkers))
+    
+    return visited_websites()
+    
